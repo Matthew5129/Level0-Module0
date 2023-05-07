570c6de9905c041bd8fa9e4c3775b716f19db985
hi <hi@hi.com>
2023-05-07 12:59:20 -0700

    first commit

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,19 +5,21 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    donkey_kong = turtle.Turtle()
 
-    # Make your turtle's shape 'turtle', .shape('turtle')
-
+    # Make your turtle shape 'turtle', .shape('turtle')
+    donkey_kong.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    donkey_kong.speed(9)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    donkey_kong.color('lime')
     # Move your turtle forward using .forward(100)
+    donkey_kong.forward(5)
     # TEST    Did your turtle move forward?
-
+    #Yes
     # Move your turtle left or right using .left(90) or .right(90)
-
+    donkey_kong.right(90)
+    for i in range:
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
 
@@ -25,6 +27,7 @@ if __name__ == '__main__':
     # x=0 and y=0 is the center of the screen
 
     # Have your turtle draw a circle using .circle(radius, steps=30)
+
     # TEST    Did your turtle draw a circle?
 
     # Add color to your shape by adding .begin_fill() before drawing the circle
