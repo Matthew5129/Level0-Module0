fbf9bac1b7d9cb76cceee5a114481d39b7015e20
jereycui2008 <jeremycuid@gmail.com>
2023-09-08 18:27:03 -0700

    0

--- _02_code_flow/_b_pentagon_crazy.py
+++ _02_code_flow/_b_pentagon_crazy.py
@@ -31,24 +31,26 @@ if __name__ == '__main__':
     # Create a variable to hold the number of sides in a pentagon
     sides = 5
     # Create a variable to be the angle of 360 divided by the sides variable
-    angle = 360
+    angle = 360/sides
     # Use a for loop to repeat ALL the following lines of code 360 times. 
-        
+    for i in range(360):
         # If the loop variable (i) is equal to 100, set the turtle width to 2
-        
+        if i==100:
+            pentagon.width(2)
         # If the loop variable (i) is equal to 200, set the turtle width to 3
-        
+        if i==200:
+            pentagon.width(3)
         # Use the get_next_color function to set the turtle pencolor,
         # *hint .pencolor(get_next_color(i))
-        
+        pentagon.pencolor(get_next_color(i))
         # Move the turtle forward by the loop variable, *hint .forward(i)
-        
+        pentagon.forward(i)
         # Turn the turtle to the right by the angle variable + 1
-
+        pentagon.right(angle+1)
     # Hide your turtle so you can see the pattern.
-        
+    pentagon.hideturtle()
     # Check the pattern against the picture in the recipe. If it matches, you are done!
-    
+    #it matches
     # Variations:
     # *Make the pattern really huge
     # *Change the colors
--- _02_code_flow/_c_flaming_ninja_star.py
+++ _02_code_flow/_c_flaming_ninja_star.py
@@ -21,39 +21,42 @@ if __name__ == '__main__':
     flameSize = 130         # the length of the flaming arms
     
     # Make a new turtle
-    
+    ninja_star = turtle.Turtle()
     # Make the turtle shape 'turtle', .shape('turtle')
-    turtle.shape('turtle')
+    ninja_star.shape('turtle')
     # Set the turtle width to 2
-    turtle.turtlesize(stretch_wid=2)
+    ninja_star.width(2)
     # Set the turtle speed to 0 (fastest)
-    turtle.speed(0)
+    ninja_star.speed(0)
     # Use a for loop to repeat all of the code below ONE time (we will change
     # this later)
-
-
+    for i in range(25):
         # TURN RIGHT     Turn the turtle 1/8 of a circle (hint: 360 degrees
         #                will turn a full circle)
-        turtle.circle(360)
+        ninja_star.right(360/8)
         # DRAW           Move the turtle 64 pixels
-        turtle.forward(64)
+        ninja_star.forward(64)
         # TURN LEFT      Turn the turtle 40 degrees to the LEFT. (Negative
         #                numbers will turn the turtle counter-clockwise.)
-        turtle.left(40)
+        ninja_star.left(40)
         # DRAW FLAME     Move the turtle the distance in the variable flameSize
+        ninja_star.fillcolor('orange')
 
+        ninja_star.begin_fill()
+
+        ninja_star.forward(flameSize)
         #                Turn the turtle to the right 170 degrees
-        turtle.right(170)
+        ninja_star.right(170)
         #                Move the turtle the distance in the variable flameSize (again)
-
+        ninja_star.forward(flameSize)
         #  TURN RIGHT    Turn the turtle 62 degrees to the right
-        turtle.right(62)
+        ninja_star.right(62)
         #  DRAW          Move the turtle the distance in the variable baseSize
-
+        ninja_star.forward(baseSize)
         # Call the turtle .end_fill() method
-        turtle.end_fill()
+        ninja_star.end_fill()
     # Hide your turtle so you can see the pattern.
-    turtle.hideturtle()
+    ninja_star.hideturtle()
     # TEST   Run the program. Check that your shape is the same as the first
     #        picture in the recipe. This is one arm of the ninja star.
 
@@ -63,6 +66,6 @@ if __name__ == '__main__':
 
     # LOOP   When you have one arm looking right, change your for loop to
     #        repeat 25 times.
-    
+
     # call the turtle .done() method
     turtle.done()
